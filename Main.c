41695b42e7cda478c46daec072c79d737f3cfe45
yaelh <elhaeey@gmail.com>
2023-08-23 17:32:29 +0100

    updated algorithm

--- Main.c
+++ Main.c
@@ -1,8 +1,10 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "TileData.c"
+#include "string.h"
 #include "BackgroundTiles.c"
 #include "testmap.c"
+#include "testmap2.c"
 #include "rand.h"
 
 #define mapWidth 32
@@ -11,72 +13,15 @@
 
 unsigned char map [1024];
 
-BOOLEAN inBounds(int16_t* tile)
-{
-	return (*tile % 32!=0 && *tile % 33 !=0);
-}
-
-int16_t getNextValidMove(int16_t currentPos)
-{
-	int8_t options = 1;
-	
-	int16_t up = currentPos - mapWidth;
-	int16_t down = currentPos + mapWidth;
-	int8_t left = currentPos - 1;
-	int8_t right = currentPos + 1;
-	options = (up > 0 && inBounds(&up))? options+1 : options;
-	options = (down < mapHeight && inBounds(&down))? options+1:  options;
-	options = (left > 0 && inBounds(&left))? options+1 : options;
-	options = (right < mapWidth && inBounds(&right))? options+1 : options;
-	
-	
-	int8_t selection = (rand() % (options + 1 - 1)) + 1;
-	switch (selection)
-	{
-	case 1:
-		return - mapWidth;
-	case 2:
-		return mapWidth;
-	case 3:
-		return -1;
-	case 4:
-		return 1;
-	default:
-		return 0;
-		
-	}	
-}
 
 void generateNewMap()
 {
-	int16_t start = rand();
-	int16_t end = rand();
-	
-	start = (start % (324 - 34 + 1)) + 34;
-	end = (end % (991- 927 +  1)) + 927;
-	start = start % 32 == 0 || start % 33 ==0 ? start + 2 : start;
-	end = end % 32 == 0  || end % 33 == 0 ? end -2 : end;
-	int16_t pos = start;
 
-	for(int16_t i=0;i< sizeof(testmap)/sizeof(testmap[0]) ;i++)
-	{
-		map[i] = BackgroundTiles[1];
-	}
-	map[start] = BackgroundTiles[0];
-	map[end] = BackgroundTiles[0];
-
-	int8_t turns = 0;
-	while(turns<100)
-	{
-		map[pos] = BackgroundTiles[0];
-		pos += getNextValidMove(pos);
-		
-		//try a splash fill? then just go over the borders
-		turns++;
-	}
+	memcpy(&map[0],&testmap[0],sizeof(map)/2); //data copies L to R use rows from different maps instead
+	memcpy(&map[sizeof(map)/2],&testmap2[105],(sizeof(map)/2));
+	//we can check the row above for a walkable tile.  then on the next line, we can verify that the row we will insert, has at least
+	//1 tile in common
 	
-	
-
 }
 
 int main()
