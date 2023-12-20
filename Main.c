368ebd6ceb0c0f51fdfac233ed1578fd6f608a30
yaelh <elhaeey@gmail.com>
2023-12-20 16:35:26 +0000

    added new character sprite and movement animations

--- Main.c
+++ Main.c
@@ -6,8 +6,7 @@
 #include "testmap.c"
 #include "testmap2.c"
 #include "testmap3.c"
-#include "char_fwd_1.h"
-#include "char_fwd_1.c"
+#include "char_1_right.c"
 #include "rand.h"
 
 #define mapWidth 32
@@ -18,14 +17,22 @@
 unsigned char map [1024];
 unsigned char* maps[3] = {testmap,testmap2,testmap3};
 
-const uint8_t MOVE_FRAME_DELAY = 5;
+const uint8_t MOVE_FRAME_DELAY = 50;
+const uint8_t ANIM_DELAY = 30;
 const uint8_t SCROLL_AMT = 16;
 
-/*sprite 0 and 1 are the character sprites drawn in 8x16 mode*/
-
-/*movement and input*/
+/* movement and input */
 uint16_t playerX=8,playerY=16;
 uint8_t joypadCurrent=0,joypadPrev=0;
+uint8_t moved = 0;
+
+/* animation */
+uint8_t current_anim = 0;
+uint16_t anim_timer = 0;
+
+/* scrolling */
+uint16_t viewportOffsetX = 0;
+uint16_t viewportOffsetY = 0;
 
 void generateNewMap()
 {
@@ -59,90 +66,13 @@ uint16_t getPlayerY()
 	return plY;
 }
 
-int main()
+/* 
+*scroll the background whenever we hit an edge
+*except when we reach the edge of the map
+*/
+void check_bkg_scroll(uint16_t viewportOffsetX, uint16_t viewportOffsetY)
 {
-
-	SHOW_BKG;
-	SHOW_SPRITES;
-	SPRITES_8x16;
-	DISPLAY_ON; 
-
-	set_bkg_data(0,tileData_NUM_TILES,TileData);
-	set_sprite_data(0,char_NUM_TILES,character);
-	initrand(DIV_REG);
-
-	generateNewMap();
-	set_bkg_tiles(0,0,testmapWidth,testmapHeight,map);
-	set_sprite_tile(0,0);
-	set_sprite_tile(1,2);
-
-	uint16_t viewportOffsetX = 0;
-	uint16_t viewportOffsetY = 0;
-	uint8_t moved = 0;
-	uint8_t frames = MOVE_FRAME_DELAY;
-	
-
-	while(1)
-	{
-		/*
-		*only allow movement once every n frames
-		*do not allow movement to invoke the screen wrap
-		*/
-		frames++;
-		if(frames >= MOVE_FRAME_DELAY)
-		{
-			moved = 0;
-			frames = 0;
-		}
-		joypadPrev = joypadCurrent;
-		joypadCurrent = joypad();
-
-		if(!moved)
-		{
-			if(joypadCurrent & J_LEFT)
-			{
-				if( getPlayerX() + viewportOffsetX > 8)
-				{
-					playerX -=8;
-					moved =1;
-					frames = 0;	
-				}			
-			} 
-			if(joypadCurrent & J_RIGHT)
-			{
-				if(getPlayerX() + viewportOffsetX < mapWidth * 8)
-				{
-					playerX +=8;
-					moved = 1;
-					frames = 0;
-				}	
-			} 
-			if(joypadCurrent & J_UP)
-			{
-				if(getPlayerY() + viewportOffsetY > 16)
-				{
-					playerY -=8;
-					moved = 1;
-					frames = 0;
-				}
-			}
-			if(joypadCurrent & J_DOWN)
-			{
-				if(getPlayerY() + viewportOffsetY < mapHeight * 8)
-				{
-					playerY +=8;
-					moved = 1;
-					frames = 0;
-				}	
-			} 
-				
-		}
-
-		/* 
-		*scroll the background whenever we hit an edge
-		*except when we reach the edge of the map
-		*/
-		if(getPlayerX() >= SCREENWIDTH)
+		if(getPlayerX() > SCREENWIDTH)
 		{
 			playerX -= SCROLL_AMT;
 			delay(100);
@@ -158,7 +88,7 @@ int main()
 
 			viewportOffsetX -= SCROLL_AMT;
 		}
-		else if(getPlayerY() >= SCREENHEIGHT)
+		else if(getPlayerY() > SCREENHEIGHT)
 		{
 			playerY-= SCROLL_AMT;
 			delay(100);
@@ -173,12 +103,101 @@ int main()
 			scroll_bkg(0, -1 * SCROLL_AMT);
 			viewportOffsetY -= SCROLL_AMT;
 		}
-		
+}
 
-		uint16_t playerXa = getPlayerX()+ charSprites_width/2;
-		move_sprite(0,playerX, playerY);
-		move_sprite(1,playerXa,playerY);
+void update_animations()
+{
+	anim_timer = anim_timer++ >= ANIM_DELAY ? 0: anim_timer; 
+	current_anim = anim_timer >= ANIM_DELAY ? !current_anim : current_anim;
+	set_sprite_tile(0,current_anim);
+}
+
+int main()
+{
+	SHOW_BKG;
+	SHOW_SPRITES;
+	SPRITES_8x8;
+	DISPLAY_ON; 
+
+	uint8_t frames = MOVE_FRAME_DELAY;
+	
+	set_bkg_data(0,tileData_NUM_TILES,TileData);
+	set_sprite_data(0,7,character);
+	initrand(DIV_REG);
+
+	generateNewMap();
+	set_bkg_tiles(0,0,testmapWidth,testmapHeight,map);
+	set_sprite_tile(0,0);
+
+	while(1)
+	{
+		update_animations();
 		
+		frames++;
+		if(frames >= MOVE_FRAME_DELAY)
+		{
+			moved = 0;
+			frames = 0;
+		}
+		joypadPrev = joypadCurrent;
+		joypadCurrent = joypad();
+
+		/*
+		*only allow movement once every n frames
+		*do not allow movement to invoke the screen wrap
+		*/
+		if(!moved)
+		{
+			if(joypadCurrent & J_LEFT)
+			{
+				if( getPlayerX() + viewportOffsetX > 8)
+				{
+					playerX -=8;
+					current_anim = 3;
+					anim_timer = 0;
+					moved = 1;
+					frames = 0;	
+				}			
+			} 
+			if(joypadCurrent & J_RIGHT)
+			{
+				if(getPlayerX() + viewportOffsetX < mapWidth * 8)
+				{
+					playerX +=8;
+					current_anim = 2;
+					anim_timer = 0;
+					moved = 1;
+					frames = 0;
+				}	
+			} 
+			if(joypadCurrent & J_UP)
+			{
+				if(getPlayerY() + viewportOffsetY > 16)
+				{
+					playerY -=8;
+					current_anim = 6;
+					anim_timer = 0;
+					moved = 1;
+					frames = 0;
+				}
+			}
+			if(joypadCurrent & J_DOWN)
+			{
+				if(getPlayerY() + viewportOffsetY < mapHeight * 8)
+				{
+					playerY +=8;
+					current_anim = 4;
+					anim_timer = 0;
+					moved = 1;
+					frames = 0;
+				}	
+			} 
+				
+		}
+
+		check_bkg_scroll(viewportOffsetX, viewportOffsetY);
+
+		move_sprite(0,playerX, playerY);
 		
 		wait_vbl_done();
 	
